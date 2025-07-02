0e169d4e3b18aa8a49dd0633398b74c53c1985ff
Aishu Voot <avootuku@gmu.edu>
2025-07-01 21:52:46 -0400

    Version 2

--- git1.py
+++ git1.py
@@ -11,3 +11,22 @@ def pattern1(num):
 print(pattern1(10))
 
 
+def pattern2(final_letter):
+    alphabet={chr(i):i-64 for i in range(65,91)}
+    seen=[]
+    for k in alphabet.keys():
+        if k==final_letter:
+         seen.append(k)
+         break
+        else:
+           seen.append(k)
+    for j in range(len(seen)):
+       print(' '*(len(seen)-j), end='')
+       if j==0:
+          print(seen[0],end='  ')
+       else:
+          for l in range(j+1):
+             if l==j:
+                print(seen[j])
+             print(seen[l],end='  ')
+      
